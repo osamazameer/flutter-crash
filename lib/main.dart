1be666637ec8e7f1809ac59b1aaf6fbe0cd244f6
osamazamir <ocamahere@gmail.com>
2021-04-26 22:40:51 +0500

    Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,34 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
+  void answerQ() {
+    questionIndex = questionIndex + 1;
+  }
+
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    var questions = [
+      'This is Question No 1',
+      'This is Question No 2',
+    ];
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Ocama'),
+            ),
+            body: Column(children: [
+              Text(questions.elementAt(0)),
+              RaisedButton(child: Text('Answer 1'), onPressed: null),
+              RaisedButton(child: Text('Answer 2'), onPressed: null),
+              RaisedButton(child: Text('Answer 3'), onPressed: null),
+            ])));
   }
 }
